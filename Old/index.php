7ecd6176205084dccfe004e890ee73edee5e5f21
aMi <rafal.zdziech@gmail.com>
2015-11-14 17:23:38 +0000

    initial init

---
+++ Old/index.php
@@ -0,0 +1,12 @@
+<?
+include('top_page.php');
+include('middle.php');
+?>
+
+</font>
+<center><p><h1>Fortune cookies available 5 for 1 pound.</h1></p><br><font color=white size=4><br>We welcome all feedback at <a href="mailto:chineselanternpz@aol.com?subject=Feedback%20from%20Chinese%20Lantern%20Website"><font color=white size=4>chineselanternpz@aol.com</font></a></font></center>
+<center><h2><a href=register.php><font color=white>Register today and get your free dishes!</font></a></h2></center>
+
+<?
+include('footer.php');
+?>
